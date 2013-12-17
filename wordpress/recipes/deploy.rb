5e5809f142cfcf989835f51c1241408f5bcb322c
David Negstad <dnegstad@gmail.com>
2013-12-17 02:36:07 -0800

    Simplify deploy recipe

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,57 +1,17 @@
-require 'uri'
-require 'net/http'
-require 'net/https'
+include_recipe 'deploy'
 
-uri = URI.parse("https://api.wordpress.org/secret-key/1.1/salt/")
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-request = Net::HTTP::Get.new(uri.request_uri)
-response = http.request(request)
-keys = response.body
-
-node[:deploy].each do |app_name, deploy|
-
-    template "#{deploy[:deploy_to]}/current/composer.json" do
-        source "composer.json.erb"
-        mode 0660
-        group deploy[:group]
-
-        if platform?("ubuntu")
-          owner "www-data"
-        elsif platform?("amazon")
-          owner "apache"
-        end
+node[:deploy].each do |application, deploy|
+    if deploy[:application_type] != 'php'
+        Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+        next
     end
-
-    script "install_composer" do
-        interpreter "bash"
-        user "root"
-        cwd "#{deploy[:deploy_to]}/current"
-        code <<-EOH
-        curl -s https://getcomposer.org/installer | php
-        php composer.phar install
-        EOH
-    end
-
+    
     template "#{deploy[:deploy_to]}/current/wp-config.php" do
-        source "wp-config.php.erb"
-        mode 0660
+        cookbook 'wordpress'
+        source 'wp-config.php.erb'
+        mode '0660'
+        owner deploy[:user]
         group deploy[:group]
-
-        if platform?("ubuntu")
-          owner "www-data"
-        elsif platform?("amazon")
-          owner "apache"
-        end
-
-        variables(
-            :database   => (deploy[:database][:database] rescue nil),
-            :user       => (deploy[:database][:username] rescue nil),
-            :password   => (deploy[:database][:password] rescue nil),
-            :host       => (deploy[:database][:host] rescue nil),
-            :keys       => (keys rescue nil)
-        )
+        variables(:database => deploy[:database])
     end
-
 end
