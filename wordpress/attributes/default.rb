47662145f8c319620ea36d7881d1c336d0174774
David Negstad <dnegstad@gmail.com>
2013-12-20 15:40:48 -0800

    Add debug option for wordpress

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -3,6 +3,8 @@ default[:wordpress][:composer][:install_command] = "curl -sS https://getcomposer
 node[:deploy].each do |application, deploy|
   next unless deploy[:application_type] == 'php'
 
+  default[:deploy][application][:wordpress][:debug] = false
+
   default[:deploy][application][:wordpress][:system_path] = 'wordpress'
   default[:deploy][application][:wordpress][:content_path] = 'wp-content'
   default[:deploy][application][:wordpress][:default_theme] = 'twentyfourteen'
