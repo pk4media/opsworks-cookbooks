05a0a2c5f19d719d2e7008bb189227b3ee4665ad
David Negstad <dnegstad@gmail.com>
2013-12-20 14:22:01 -0800

    Update deploy recipe

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -46,96 +46,102 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  # Configure the w3-total-cache settings
-  template "#{deploy[:deploy_to]}/shared/config/master.php" do
-    cookbook 'wordpress'
-    source 'master.php.erb'
-    mode '0660'
-    owner deploy[:user]
-    group deploy[:group]
-    variables(:memcached => deploy[:memcached], :cache => deploy[:wordpress][:cache])
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config")
+  if deploy[:wordpress][:cache][:enabled]
+    # Configure the w3-total-cache settings
+    template "#{deploy[:deploy_to]}/shared/config/master.php" do
+      cookbook 'wordpress'
+      source 'master.php.erb'
+      mode '0660'
+      owner deploy[:user]
+      group deploy[:group]
+      variables(:memcached => deploy[:memcached], :cache => deploy[:wordpress][:cache])
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && File.exists?("#{deploy[:deploy_to]}/shared/config")
+      end
     end
-  end
 
-  link "#{deploy[:deploy_to]}/current/wp-content/w3tc-config/master.php" do
-    to "#{deploy[:deploy_to]}/shared/config/master.php"
+    link "#{deploy[:deploy_to]}/current/wp-content/w3tc-config/master.php" do
+      to "#{deploy[:deploy_to]}/shared/config/master.php"
 
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config/master.php")
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && File.exists?("#{deploy[:deploy_to]}/shared/config/master.php")
+      end
     end
-  end
 
-  template "#{deploy[:deploy_to]}/shared/config/master-admin.php" do
-    cookbook 'wordpress'
-    source 'master-admin.php.erb'
-    mode '0660'
-    owner deploy[:user]
-    group deploy[:group]
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    template "#{deploy[:deploy_to]}/shared/config/master-admin.php" do
+      cookbook 'wordpress'
+      source 'master-admin.php.erb'
+      mode '0660'
+      owner deploy[:user]
+      group deploy[:group]
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && File.exists?("#{deploy[:deploy_to]}/shared/config")
+      end
     end
-  end
-  
-  link "#{deploy[:deploy_to]}/current/wp-content/w3tc-config/master-admin.php" do
-    to "#{deploy[:deploy_to]}/shared/config/master-admin.php"
+    
+    link "#{deploy[:deploy_to]}/current/wp-content/w3tc-config/master-admin.php" do
+      to "#{deploy[:deploy_to]}/shared/config/master-admin.php"
 
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}/shared/config/master-admin.php")
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && File.exists?("#{deploy[:deploy_to]}/shared/config/master-admin.php")
+      end
     end
-  end
 
-  template "#{deploy[:deploy_to]}/current/wp-content/plugins/w3tc-wp-loader.php" do
-    cookbook 'wordpress'
-    source 'w3tc-wp-loader.php.erb'
-    mode '0660'
-    owner deploy[:user]
-    group deploy[:group]
-    variables(:deploy_to => deploy[:deploy_to])
-  end
-  
-  directory "#{deploy[:deploy_to]}/shared/cache/config" do
-    recursive true
-    action :delete
-  end
-  
-  directory "#{deploy[:deploy_to]}/shared/cache/minify" do
-    recursive true
-    action :delete
-  end
+    template "#{deploy[:deploy_to]}/current/wp-content/plugins/w3tc-wp-loader.php" do
+      cookbook 'wordpress'
+      source 'w3tc-wp-loader.php.erb'
+      mode '0660'
+      owner deploy[:user]
+      group deploy[:group]
+      variables(:deploy_to => deploy[:deploy_to])
 
-  file "#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/advanced-cache.php" do
-    content ::File.open("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/advanced-cache.php").read
-    owner deploy[:user]
-    group deploy[:group]
-    mode '0660'
-    action :create
-
-    only_if do
-      File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/advanced-cache.php")
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && 
+      end
     end
-  end
-
-  file "#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/db.php" do
-    content ::File.open("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/db.php").read
-    owner deploy[:user]
-    group deploy[:group]
-    mode '0660'
-    action :create
-    only_if do
-      deploy[:wordpress][:cache][:dbcache][:enabled] && File.exists("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/db.php")
+    
+    directory "#{deploy[:deploy_to]}/shared/cache/config" do
+      recursive true
+      action :delete
     end
-  end
+    
+    directory "#{deploy[:deploy_to]}/shared/cache/minify" do
+      recursive true
+      action :delete
+    end
 
-  file "#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/object-cache.php" do
-    content ::File.open("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/object-cache.php").read
-    owner deploy[:user]
-    group deploy[:group]
-    mode '0660'
-    action :create
-    only_if do
-      deploy[:wordpress][:cache][:objectcache][:enabled] && File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/object-cache.php")
+    file "#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/advanced-cache.php" do
+      content ::File.open("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/advanced-cache.php").read
+      owner deploy[:user]
+      group deploy[:group]
+      mode '0660'
+      action :create
+
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/advanced-cache.php")
+      end
+    end
+
+    file "#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/db.php" do
+      content ::File.open("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/db.php").read
+      owner deploy[:user]
+      group deploy[:group]
+      mode '0660'
+      action :create
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && deploy[:wordpress][:cache][:dbcache][:enabled] && File.exists("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/db.php")
+      end
+    end
+
+    file "#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/object-cache.php" do
+      content ::File.open("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/object-cache.php").read
+      owner deploy[:user]
+      group deploy[:group]
+      mode '0660'
+      action :create
+      only_if do
+        deploy[:wordpress][:cache][:enabled] && deploy[:wordpress][:cache][:objectcache][:enabled] && File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:wordpress][:content_path]}/plugins/w3-total-cache/wp-content/object-cache.php")
+      end
     end
   end
 end
