fd56f329fadb7e900b76cbcf44f8e9f488cebd5c
David Negstad <dnegstad@gmail.com>
2013-12-19 18:03:46 -0800

    Missed a config item

--- w3_total_cache/attributes/dbcache.rb
+++ w3_total_cache/attributes/dbcache.rb
@@ -4,5 +4,5 @@ node[:deploy].each do |application, deploy|
   default[:deploy][application][:wordpress][:cache][:dbcache][:debug] = false
   default[:deploy][application][:wordpress][:cache][:dbcache][:engine] = 'file'
   default[:deploy][application][:wordpress][:cache][:dbcache][:lifetime] = 180
-  default[:deploy][application][:wordpress][:cache][:dbcache][:persistant] = true
+  default[:deploy][application][:wordpress][:cache][:dbcache][:memcached][:persistant] = true
 end
