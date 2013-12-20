3e88d0c490c30dd2b616edca9155fb0d5e110f49
David Negstad <dnegstad@gmail.com>
2013-12-20 00:34:41 -0800

    Metadata again

---
+++ w3_total_cache/metadata.rb
@@ -0,0 +1,7 @@
+maintainer "PK4 Media, Inc."
+description "w3-total-cache plugin for Wordpress"
+version "0.1"
+recipe "w3_total_cache::configure", "Configure the w3-total-cache Wordpress plugin"
+recipe "w3_total_cache::deploy", "Deployment related configuration for the w3-total-cache plugin"
+
+depends "wordpress"
