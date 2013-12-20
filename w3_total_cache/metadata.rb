20336556b7ff232d6b935b82ed6da3468fe74b4f
David Negstad <dnegstad@gmail.com>
2013-12-19 23:21:57 -0800

    Update metadata

--- w3_total_cache/metadata.rb
+++ w3_total_cache/metadata.rb
@@ -1,7 +1,7 @@
 maintainer "PK4 Media, Inc."
 description "Configure W3 Total Cache Plugin"
 version "0.1"
-recipe "w3_total_cache::configure"
-recipe "w3_total_cache::deploy"
+recipe "w3_total_cache::configure", "Configure w3-total-cache plugin settings"
+recipe "w3_total_cache::deploy", "Deploy w3-total-cache plugin files"
 
 depends "wordpress"
--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -1,7 +1,7 @@
 maintainer "PK4 Media, Inc."
 description "Configure Wordpress"
 version "0.1"
-recipe "wordpress::configure"
-recipe "wordpress::deploy"
+recipe "wordpress::configure", "Configure wordpress settings"
+recipe "wordpress::deploy", "Deploy wordpress"
 
 depends "deploy"
