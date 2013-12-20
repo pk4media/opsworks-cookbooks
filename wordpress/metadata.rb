33adc564b13875ec16e3de83f99b345e9c8c81ad
David Negstad <dnegstad@gmail.com>
2013-12-19 23:30:41 -0800

    Remove metadata

--- wordpress/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer "PK4 Media, Inc."
-description "Configure Wordpress"
-version "0.1"
-recipe "wordpress::configure", "Configure wordpress settings"
-recipe "wordpress::deploy", "Deploy wordpress"
-
-depends "deploy"
