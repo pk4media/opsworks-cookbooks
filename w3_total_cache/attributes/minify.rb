9440676c485254a55c462b2c9b3342c99c4ef694
David Negstad <dnegstad@gmail.com>
2013-12-19 16:57:08 -0800

    Add extra level to minify auto enabled

--- w3_total_cache/attributes/minify.rb
+++ w3_total_cache/attributes/minify.rb
@@ -4,7 +4,7 @@ node[:deploy].each do |application, deploy|
   default[:deploy][application][:wordpress][:cache][:minify][:enabled] = false
   default[:deploy][application][:wordpress][:cache][:minify][:debug] = false
   default[:deploy][application][:wordpress][:cache][:minify][:engine] = 'file'
-  default[:deploy][application][:wordpress][:cache][:minify][:auto] = true
+  default[:deploy][application][:wordpress][:cache][:minify][:auto][:enabled] = true
   default[:deploy][application][:wordpress][:cache][:minify][:auto][:disable_filename_length_test] = true
-  default[:deploy][application][:wordpress][:cache][:minify][:auot][:filename_length] = 150
+  default[:deploy][application][:wordpress][:cache][:minify][:auto][:filename_length] = 150
 end
