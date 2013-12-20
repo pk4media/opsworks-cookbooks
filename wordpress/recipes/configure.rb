60a42b759726b3fde47548fc30cd5715e6142114
David Negstad <dnegstad@gmail.com>
2013-12-19 23:42:36 -0800

    Missing end

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -2,13 +2,14 @@ node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'php'
     Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
     next
-
-    template "#{deploy[:deploy_to]}/shared/config/wp-config.php" do
-      cookbook 'wordpress'
-      source 'wp-config.php.erb'
-      mode '0660'
-      owner deploy[:user]
-      group deploy[:group]
-      variables(:database => deploy[:database], :wordpress => deploy[:wordpress])
-    end
   end
+
+  template "#{deploy[:deploy_to]}/shared/config/wp-config.php" do
+    cookbook 'wordpress'
+    source 'wp-config.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:database => deploy[:database], :wordpress => deploy[:wordpress])
+  end
+end
