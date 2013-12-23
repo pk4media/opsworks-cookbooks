2e5f9d5c6ec419b09db3526fe4887796986fbede
David Negstad <dnegstad@gmail.com>
2013-12-22 20:17:24 -0800

    Allow minify rewrite settings to be overriden

--- w3_total_cache/attributes/minify.rb
+++ w3_total_cache/attributes/minify.rb
@@ -3,6 +3,7 @@ node[:deploy].each do |application, deploy|
 
   default[:deploy][application][:wordpress][:cache][:minify][:debug] = false
   default[:deploy][application][:wordpress][:cache][:minify][:engine] = 'file'
+  default[:deploy][application][:wordpress][:cache][:minify][:rewrite] = true
   default[:deploy][application][:wordpress][:cache][:minify][:auto][:enabled] = true
   default[:deploy][application][:wordpress][:cache][:minify][:auto][:disable_filename_length_test] = true
   default[:deploy][application][:wordpress][:cache][:minify][:auto][:filename_length] = 150
