93ca1c3fbd808dda63f57c1758ecf329b86e8268
David Negstad <dnegstad@gmail.com>
2013-12-23 00:16:33 -0800

    Add max filesize setting

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -4,6 +4,7 @@ default[:wordpress][:composer][:install_command] = "curl -sS https://getcomposer
 node[:deploy].each do |application, deploy|
   default[:deploy][application][:wordpress][:debug] = false
   default[:deploy][application][:wordpress][:default_theme] = 'twentyfourteen'
+  default[:deploy][application][:wordpress][:upload_max_filesize] = '2M'
 end
 
 include_attribute 'w3_total_cache'
