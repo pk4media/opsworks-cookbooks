3fa325efe39e2f91d071ec6fe50e34d29e5de9e0
David Negstad <dnegstad@gmail.com>
2014-01-06 12:31:01 -0800

    Fix bad attribute names

--- manager/attributes/default.rb
+++ manager/attributes/default.rb
@@ -1,4 +1,6 @@
-default[:cron][:report_reconcile][:minutes] = '*/1'
-default[:cron][:report_reconcile][:hours] = '*'
+node[:deploy].each do |application, deploy|
+  default[application][:cron][:report_reconcile][:minutes] = '*/1'
+  default[application][:cron][:report_reconcile][:hours] = '*'
+end
 
 include_attribute "manager::config"
