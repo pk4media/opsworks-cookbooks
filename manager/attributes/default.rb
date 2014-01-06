4e64627e222b8fa34fdeaf579de3323360cba876
David Negstad <dnegstad@gmail.com>
2014-01-06 12:25:42 -0800

    Update rollup cronjob recipe

--- manager/attributes/default.rb
+++ manager/attributes/default.rb
@@ -1,5 +1,4 @@
-default[:cron][:report_reconcile][:minutes] = '*/2'
+default[:cron][:report_reconcile][:minutes] = '*/1'
 default[:cron][:report_reconcile][:hours] = '*'
-default[:cron][:report_reconcile][:command] = '/srv/www/xpsmanager/shared/scripts/rollup.sh'
 
 include_attribute "manager::config"
--- manager/recipes/cronjobs.rb
+++ manager/recipes/cronjobs.rb
@@ -1,5 +1,16 @@
-cron 'report_reconcile' do
-  minute node[:cron][:report_reconcile][:minutes]
-  hour node[:cron][:report_reconcile][:hours]
-  command node[:cron][:report_reconcile][:command]
+node[:deploy].each do |application, deploy|
+  template "#{deploy[:deploy_to]}/shared/scripts/report_reconcile.sh" do
+    cookbook 'manager'
+    source 'report_reconcile.sh.erb'
+    mode '0750'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_report_reconcile_lock")
+  end
+
+  cron "#{application}_report_reconcile" do
+    minute node[application][:cron][:report_reconcile][:minutes]
+    hour node[application][:cron][:report_reconcile][:hours]
+    command "#{deploy[:deploy_to]}/shared/scripts/report_reconcile.sh"
+  end
 end
