365d4375afabda145398489694d4d7496788b9c6
David Negstad <dnegstad@gmail.com>
2014-01-06 00:19:51 -0800

    Update cookbook command

--- manager/attributes/default.rb
+++ manager/attributes/default.rb
@@ -1,3 +1,3 @@
 default[:cron][:report_reconcile][:minutes] = '*/2'
 default[:cron][:report_reconcile][:hours] = '*'
-default[:cron][:report_reconcile][:command] = "cd /srv/www/xpsmanager/current && RAILS_ENV=production bundle exec rake adserver:reports:update --silent"
+default[:cron][:report_reconcile][:command] = '/srv/www/xpsmanager/shared/scripts/rollup.sh'
