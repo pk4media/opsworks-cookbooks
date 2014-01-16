16ab9916c2cfda3b84efeaa1576c7249a166e90c
David Negstad <dnegstad@gmail.com>
2014-01-16 02:38:16 -0800

    Handle restart once deployed

--- server/recipes/deploy.rb
+++ server/recipes/deploy.rb
@@ -15,4 +15,14 @@ node[:deploy].each do |application, deploy|
       ::File.exists?("#{deploy[:deploy_to]}/shared/config/application.yml")
     end
   end
+
+  execute "restart_rails" do
+    cwd "#{deploy[:deploy_to]}/current"
+    command "sleep #{deploy[:sleep_before_restart]} && #{node[:opsworks][:rails_stack][:restart_command]}"
+    action :run
+
+    only_if do 
+      File.exists?("#{deploy[:deploy_to]}/current")
+    end
+  end
 end
