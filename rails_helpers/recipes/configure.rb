f5a544c338440428ea062609c190fc36a18151aa
David Negstad <dnegstad@gmail.com>
2014-01-17 23:41:40 -0800

    Use ruby block

--- rails_helpers/recipes/configure.rb
+++ rails_helpers/recipes/configure.rb
@@ -1,8 +1,16 @@
 node[:deploy].each do |application, deploy|
-  config = Hash[deploy[:rails_env], deploy[:rails_app][:config].to_hash] if deploy[:rails_app][:write_config]
-  File.open(File.join(deploy[:deploy_to], 'shared', 'config', 'application.yml'), 'w', 0640) do |file|
-    file.write YAML.dump(config)
-  end if config
+  ruby_block 'application.yml' do
+    block do
+      config = Hash[deploy[:rails_env], deploy[:rails_app][:config].to_hash]
+      ::File.open(::File.join(deploy[:deploy_to], 'shared', 'config', 'application.yml'), 'w', 0640) do |file|
+        file.write YAML.dump(config)
+      end
+    end
+
+    only_if do
+      deploy[:rails_app][:write_config]
+    end
+  end
 
   template "#{deploy[:deploy_to]}/shared/config/aws.yml" do
     Chef::Log.debug "Adding aws config for #{application}"
