3dab057263cb0b7f6550a01d84a515224d554696
David Negstad <dnegstad@gmail.com>
2014-01-28 01:22:49 -0800

    Update cronjobs for manager

--- manager/recipes/cronjobs.rb
+++ manager/recipes/cronjobs.rb
@@ -13,4 +13,90 @@ node[:deploy].each do |application, deploy|
     hour node[application][:cron][:report_reconcile][:hours]
     command "#{deploy[:deploy_to]}/shared/scripts/report_reconcile.sh"
   end
+
+  template "#{deploy[:deploy_to]}/shared/scripts/display_rolling.sh" do
+    cookbook 'manager'
+    source 'display_rolling.sh.erb'
+    mode 0750
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_display_rolling_lock")
+  end
+
+  cron "#{application}_display_rolling" do
+    minute '*/2'
+    command "#{deploy[:deploy_to]}/shared/scripts/display_rolling.sh"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/scripts/video_rolling.sh" do
+    cookbook 'manager'
+    source 'video_rolling.sh.erb'
+    mode 0750
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_video_rolling_lock")
+  end
+
+  cron "#{application}_video_rolling" do
+    minute '*/2'
+    command "#{deploy[:deploy_to]}/shared/scripts/video_rolling.sh"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/scripts/centaur_rolling.sh" do
+    cookbook 'manager'
+    source 'centaur_rolling.sh.erb'
+    mode 0750
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_centaur_rolling_lock")
+  end
+
+  cron "#{application}_centaur_rolling" do
+    minute '*/5'
+    command "#{deploy[:deploy_to]}/shared/scripts/centaur_rolling.sh"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/scripts/scope_rolling.sh" do
+    cookbook 'manager'
+    source 'scope_rolling.sh.erb'
+    mode 0750
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_scope_rolling_lock")
+  end
+
+  cron "#{application}_scope_rolling" do
+    minute '*/5'
+    command "#{deploy[:deploy_to]}/shared/scripts/scope_rolling.sh"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/scripts/display_daily.sh" do
+    cookbook 'manager'
+    source 'display_daily.sh.erb'
+    mode 0750
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_display_daily_lock")
+  end
+
+  cron "#{application}_display_daily" do
+    minute '0'
+    hour '8'
+    command "#{deploy[:deploy_to]}/shared/scripts/display_daily.sh"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/scripts/video_daily.sh" do
+    cookbook 'manager'
+    source 'video_daily.sh.erb'
+    mode 0750
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :current => "#{deploy[:deploy_to]}/current", :lock => "#{application}_video_daily_lock")
+  end
+
+  cron "#{application}_video_daily" do
+    minute '0'
+    hour '8'
+    command "#{deploy[:deploy_to]}/shared/scripts/video_daily.sh"
+  end
 end
