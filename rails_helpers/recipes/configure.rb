75c2b298c8007fafe05093e6d7ee7bed40075fb0
David Negstad <dnegstad@gmail.com>
2014-02-20 12:55:04 -0800

    Update cookbook to create couchbase.yml config

--- rails_helpers/recipes/configure.rb
+++ rails_helpers/recipes/configure.rb
@@ -7,7 +7,18 @@ node[:deploy].each do |application, deploy|
     mode 0640
 
     only_if do
-      deploy[:rails_app][:write_config]
+      deploy[:rails_app][:write_config] && deploy[:rails_app][:config]
+    end
+  end
+
+  file "#{deploy[:deploy_to]}/shared/config/couchbase.yml" do
+    content YAML.dump(Hash[deploy[:rails_env], deploy[:rails_app][:couchbase].to_hash]).gsub('!map:Chef::Node::ImmutableMash','').gsub('!seq:Chef::Node::ImmutableArray','').gsub('---','')
+    owner deploy[:user]
+    group deploy[:group]
+    mode 0640
+
+    only_if do
+      deploy[:rails_app][:write_config] && deploy[:rails_app][:couchbase]
     end
   end
 
