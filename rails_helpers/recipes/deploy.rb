5f6bee9faf3e68701c900c6e2fdb0d068647cfa4
David Negstad <dnegstad@gmail.com>
2014-03-14 20:13:43 -0700

    Update deployment cookbook

--- rails_helpers/recipes/deploy.rb
+++ rails_helpers/recipes/deploy.rb
@@ -12,6 +12,17 @@ node[:deploy].each do |application, deploy|
     end
   end
 
+  file "#{deploy[:deploy_to]}/shared/config/application.yml" do
+    content YAML.dump(Hash[deploy[:rails_env], deploy[:rails_app][:config].to_hash]).gsub('!map:Chef::Node::ImmutableMash','').gsub('!seq:Chef::Node::ImmutableArray','').gsub('---','')
+    owner deploy[:user]
+    group deploy[:group]
+    mode 0640
+
+    only_if do
+      deploy[:rails_app][:write_config] && deploy[:rails_app][:config]
+    end
+  end
+
   link "#{deploy[:deploy_to]}/current/config/application.yml" do
     to "#{deploy[:deploy_to]}/shared/config/application.yml"
 
@@ -29,11 +40,36 @@ node[:deploy].each do |application, deploy|
     end
   end
 
+  file "#{deploy[:deploy_to]}/shared/config/couchbase.yml" do
+    content YAML.dump(Hash[deploy[:rails_env], deploy[:rails_app][:couchbase].to_hash]).gsub('!map:Chef::Node::ImmutableMash','').gsub('!seq:Chef::Node::ImmutableArray','').gsub('---','')
+    owner deploy[:user]
+    group deploy[:group]
+    mode 0640
+
+    only_if do
+      deploy[:rails_app][:write_config] && deploy[:rails_app][:couchbase]
+    end
+  end
+
   link "#{deploy[:deploy_to]}/current/config/couchbase.yml" do
     to "#{deploy[:deploy_to]}/shared/config/couchbase.yml"
 
     only_if do
       ::File.exists?("#{deploy[:deploy_to]}/shared/config/couchbase.yml")
+    end
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/aws.yml" do
+    Chef::Log.debug "Adding aws config for #{application}"
+    cookbook 'rails_helpers'
+    source 'aws.yml.erb'
+    mode '0640'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:environment => deploy[:rails_env], :aws => deploy[:aws])
+
+    only_if do
+      deploy[:aws][:access_key_id] && deploy[:aws][:secret_access_key]
     end
   end
 
