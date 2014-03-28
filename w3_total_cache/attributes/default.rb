9fba2b2ec46ed6ff7129ef55ce0cdb70f198f3c7
David Negstad <dnegstad@gmail.com>
2014-03-27 17:37:24 -0700

    Try re-ordering the attribute includes

--- w3_total_cache/attributes/default.rb
+++ w3_total_cache/attributes/default.rb
@@ -1,5 +1,12 @@
 include_attribute 'wordpress'
 
+include_attribute 'w3_total_cache::dbcache'
+include_attribute 'w3_total_cache::objectcache'
+include_attribute 'w3_total_cache::pgcache'
+include_attribute 'w3_total_cache::minify'
+include_attribute 'w3_total_cache::cdn'
+include_attribute 'w3_total_cache::browsercache'
+
 node[:deploy].each do |application, deploy|
   default[:deploy][application][:wordpress][:cache][:enabled] = false
   default[:deploy][application][:wordpress][:cache][:version] = '0.9.3'
@@ -12,10 +19,3 @@ node[:deploy].each do |application, deploy|
   default[:deploy][application][:wordpress][:cache][:cdn][:enabled] = false
   default[:deploy][application][:wordpress][:cache][:mobile][:enabled] = false
 end
-
-include_attribute 'w3_total_cache::dbcache'
-include_attribute 'w3_total_cache::objectcache'
-include_attribute 'w3_total_cache::pgcache'
-include_attribute 'w3_total_cache::minify'
-include_attribute 'w3_total_cache::cdn'
-include_attribute 'w3_total_cache::browsercache'
