6cfb95c9be893ecc59bede149b36c267bc274a40
David Negstad <dnegstad@gmail.com>
2014-06-02 18:22:44 -0700

    Update the range of config options

--- w3_total_cache/attributes/browsercache.rb
+++ w3_total_cache/attributes/browsercache.rb
@@ -1,3 +1,5 @@
 node[:deploy].each do |application, deploy|
   default[:deploy][application][:wordpress][:cache][:browsercache][:cssjs][:compression] = true
+  default[:deploy][application][:wordpress][:cache][:browsercache][:cssjs][:replace] = false
+  default[:deploy][application][:wordpress][:cache][:browsercache][:other][:replace] = false
 end
